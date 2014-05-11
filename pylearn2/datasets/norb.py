89ff381df4c3bd681202ec8e62edd5904a5907ce
Matthew Koichi Grimes <mkg@alum.mit.edu>
2014-05-11 17:34:13 +0100

    revert erroneous change to SmallNORB._categories

--- pylearn2/datasets/norb.py
+++ pylearn2/datasets/norb.py
@@ -50,8 +50,7 @@ class SmallNORB(DenseDesignMatrix):
     # datasets.preprocessing.Downsample
     original_image_shape = (96, 96)
 
-    _categories = ['blank',  # no object; background & distractor only
-                   'animal',  # four-legged animal
+    _categories = ['animal',  # four-legged animal
                    'human',  # human figure
                    'airplane',
                    'truck',
