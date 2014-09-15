8564ef913b3521dcf4caf22080f6276c4ed62e36
Matthew Koichi Grimes <mkg@alum.mit.edu>
2014-09-15 17:14:08 +0100

    wrote LocalizerDisplay, not yet tested, ClassifierDisplay confirmed to still be working.

--- pylearn2/scripts/papers/maxout/norb/demos/video_demo.py
+++ pylearn2/scripts/papers/maxout/norb/demos/video_demo.py
@@ -11,7 +11,7 @@ import sys, argparse, os, shutil, time
 import numpy, matplotlib, cv2, theano
 from matplotlib import pyplot
 from pylearn2.utils import serial, safe_zip
-from pylearn2.space import VectorSpace, CompositeSpace
+from pylearn2.space import VectorSpace, Conv2DSpace, CompositeSpace
 from pylearn2.models.mlp import MLP
 from pylearn2.datasets import new_norb
 from pylearn2.datasets.preprocessing import ZCA, LeCunLCN
@@ -54,6 +54,12 @@ def parse_args():
                         default=2,
                         type=int,
                         help="Detection window scale.")
+
+    parser.add_argument("--localize",
+                        action='store_true',
+                        default=False,
+                        help="Perform localization.")
+
     result = parser.parse_args()
 
     if (result.model is None) != (result.preprocessor is None):
@@ -104,6 +110,27 @@ class VideoDisplay(object):
 
 class ClassifierDisplay(object):
 
+    def _load_model_function(self, model_path):
+        model = serial.load(model_path)
+
+        # This is just a sanity check. It's not necessarily true; it's just
+        # expected to be true in the current use case.
+        assert isinstance(model, MLP)
+
+        input_space = model.get_input_space()
+        floatX = theano.config.floatX
+        input_symbol = input_space.make_theano_batch(name='features',
+                                                     dtype=floatX,
+                                                     batch_size=1)
+        output_symbol = model.fprop(input_symbol)
+        return theano.function([input_symbol, ], output_symbol), input_space
+
+        # specs = (model.input_space, 'features')
+        # specs = (CompositeSpace((model.input_space,
+        #                          VectorSpace(dim=test_set.y.shape[1]))),
+        #          ('features', 'targets'))
+
+
     def __init__(self, model_path, preprocessor, object_scale):
 
         # # debug stuff
@@ -120,27 +147,7 @@ class ClassifierDisplay(object):
 
         self.frame_number = 0
 
-        def load_model_function(model_path):
-            model = serial.load(model_path)
-
-            # This is just a sanity check. It's not necessarily true; it's just
-            # expected to be true in the current use case.
-            assert isinstance(model, MLP)
-
-            input_space = model.get_input_space()
-            floatX = theano.config.floatX
-            input_symbol = input_space.make_theano_batch(name='features',
-                                                         dtype=floatX,
-                                                         batch_size=1)
-            output_symbol = model.fprop(input_symbol)
-            return theano.function([input_symbol, ], output_symbol), input_space
-
-            # specs = (model.input_space, 'features')
-            # specs = (CompositeSpace((model.input_space,
-            #                          VectorSpace(dim=test_set.y.shape[1]))),
-            #          ('features', 'targets'))
-
-        self.model_function, input_space = load_model_function(model_path)
+        self.model_function, input_space = self._load_model_function(model_path)
 
         def get_preprocessor(preprocessor, image_shape=None):
             if preprocessor.endswith('.pkl'):
@@ -185,13 +192,18 @@ class ClassifierDisplay(object):
         self.margin = 10  # space between images in self.status_pixels
         self.object_scale = object_scale  # scales object detection region size
 
-        # TODO: replace this by reading the input space from the model.
+        # self.norb_image_shape = input_space.shape
+        # assert len(self.norb_image_shape) == 2
+        # TODO: replace this by reading the input space from the model
         self.norb_image_shape = numpy.asarray((96, 96)
                                               if model_path.find('small')
                                               else (106, 106))
 
         self.all_pixels = None  # set by _init_pixels
         self.model_input_dataset = None
+
+    def _get_object_shape(self):
+        return self.norb_image_shape * self.object_scale
 
     def _init_pixels(self, video_frame):
         self.all_pixels = numpy.zeros(shape=(video_frame.shape[0],
@@ -202,7 +214,7 @@ class ClassifierDisplay(object):
         self.video_pixels = self.all_pixels[:, :video_frame.shape[1], :]
         self.status_pixels = self.all_pixels[:, video_frame.shape[1]:, :]
 
-        object_shape = self.norb_image_shape * self.object_scale
+        object_shape = self._get_object_shape()
         self.object_min_corner = (numpy.asarray(self.video_pixels.shape[:2]) -
                                   object_shape) / 2
         self.object_max_corner = self.object_min_corner + object_shape
@@ -251,6 +263,35 @@ class ClassifierDisplay(object):
         self.probability_pixels_list = tuple(get_grid_window_pixels(2, c)
                                              for c in range(num_candidates))
 
+    def _preprocess(self, model_input):
+        image_shape = model_input.shape
+
+        assert str(model_input.dtype) == 'uint8'
+
+        # flatten into a design matrix
+        model_input = numpy.asarray(model_input.reshape(1, -1),
+                                    dtype=theano.config.floatX)
+        model_input /= 255.0
+
+        # if isinstance(self.preprocessor, ZCA):
+        # TODO: in create_instance_dataset.py, use the GCN preprocessor
+        # class rather than the GCN funtion as below, and save it along
+        # with the ZCA preprocessor. This will require you to add an
+        # in-place option to the GCN preprocesssor class.
+        global_contrast_normalize(model_input, scale=55.0, in_place=True)
+
+        if self.model_input_dataset is None:
+            view_converter = DefaultViewConverter(image_shape + (1,),
+                                                  axes=('b', 0, 1, 'c'))
+            self.model_input_dataset = \
+                DenseDesignMatrix(X=model_input,
+                                  view_converter=view_converter)
+        else:
+            self.model_input_dataset.X = model_input
+
+        self.preprocessor.apply(self.model_input_dataset, can_fit=False)
+        return self.model_input_dataset.X.reshape(image_shape)
+
     def update(self, video_frame):
         if self.all_pixels is None:
             self._init_pixels(video_frame)
@@ -269,37 +310,7 @@ class ClassifierDisplay(object):
                       2)  # thickness
 
         model_input = cv2.resize(gray_object, tuple(self.norb_image_shape))
-
-        def preprocess(model_input):
-            image_shape = model_input.shape
-
-            assert str(model_input.dtype) == 'uint8'
-
-            # flatten into a design matrix
-            model_input = numpy.asarray(model_input.reshape(1, -1),
-                                        dtype=theano.config.floatX)
-            model_input /= 255.0
-
-            # if isinstance(self.preprocessor, ZCA):
-            # TODO: in create_instance_dataset.py, use the GCN preprocessor
-            # class rather than the GCN funtion as below, and save it along
-            # with the ZCA preprocessor. This will require you to add an
-            # in-place option to the GCN preprocesssor class.
-            global_contrast_normalize(model_input, scale=55.0, in_place=True)
-
-            if self.model_input_dataset is None:
-                view_converter = DefaultViewConverter(image_shape + (1,),
-                                                      axes=('b', 0, 1, 'c'))
-                self.model_input_dataset = \
-                    DenseDesignMatrix(X=model_input,
-                                      view_converter=view_converter)
-            else:
-                self.model_input_dataset.X = model_input
-
-            self.preprocessor.apply(self.model_input_dataset, can_fit=False)
-            return self.model_input_dataset.X.reshape(image_shape)
-
-        model_input = preprocess(model_input)
+        model_input = self._preprocess(model_input)
         model_input = model_input[numpy.newaxis, :, :, numpy.newaxis]
         assert model_input.shape == (1, 96, 96, 1), str(model_input)
 
@@ -370,6 +381,142 @@ class ClassifierDisplay(object):
         self.frame_number += 1
 
 
+class LocalizerDisplay(ClassifierDisplay):
+
+    def __init__(self, model_path, preprocessor, object_scale):
+        super(LocalizerDisplay, self).__init__(model_path,
+                                               preprocessor,
+                                               object_scale)
+
+    # def _load_model_function(self, model_path):
+    #     # this will be replaced by a bigger model
+    #     model = serial.load(model_path)
+    # def _get_bigger_model(self,
+
+    def _get_object_shape(self):
+        object_width = min(self.video_pixels.shape[:2])
+        return numpy.asarray((object_width, object_width), dtype='int')
+
+    def _init_pixels(self, video_frame):
+        super(LocalizerDisplay, self)._init_pixels(video_frame)
+        #del self.model_input_pixels
+
+    def get_model_scale(self, model):
+        convolutional_layers = [x for x in model.layers
+                                if isinstance(x.get_output_space(),
+                                              Conv2DSpace)]
+        scales = numpy.asarray([get_scale(x) for x in model.layers])
+        scale = numpy.prod(scales, axis=0)
+        assert scale[0] == scale[1]
+        return scale
+
+    def update(self, video_frame):
+        if self.all_pixels is None:
+            self._init_pixels(video_frame)
+
+        self.video_pixels[...] = video_frame
+
+        # greyify object detection region.
+        gray_object = cv2.cvtColor(self.object_pixels, cv2.COLOR_BGR2GRAY)
+        self.object_pixels[...] = gray_object[..., numpy.newaxis]
+
+        # Draw black border around object detection region.
+        cv2.rectangle(self.video_pixels,
+                      tuple(reversed(self.object_min_corner)),
+                      tuple(reversed(self.object_max_corner)),
+                      (0, 0, 0),  # color
+                      2)  # thickness
+
+        scaled_object_shape = self._get_object_shape // self.object_scale
+        scaled_object_shape.append
+        assert len(scaled_object_shape) == 3
+        assert scaled_object_shape.dtype == numpy.dtype('int')
+        assert (scaled_object_shape > 0).all()
+
+        model_input = cv2.resize(gray_object, tuple(scaled_object_shape))
+        model_input = self._preprocess(model_input)
+        model_input = model_input[numpy.newaxis, :, :, numpy.newaxis]
+
+        softmax_map = self.model_function(model_input)
+        no_bkg_softmax_map = softmax_map[..., :50]
+        no_bkg_max_location = numpy.unravel_index(no_bkg_softmax_map.argmax(),
+                                                  no_bkg_softmax_map.shape)
+
+        # only bother showing classification if it beats out the background.
+        if no_bkg_max_location[-1] != 50:
+            softmax_map_shape = numpy.asarray(softmax_map.shape[:2],
+                                              dtype=float)
+
+            centered_max_location = (no_bkg_max_location[:2] -
+                                     (softmax_map_shape // 2))
+
+            softmax_map_scale = get_model_scale(model)
+            centered_max_location *= softmax_map_scale
+            input_center = (self.object_min_corner +
+                            self.object_max_corner) // 2
+
+            # absolute coordinates of object center
+            max_location = numpy.asarray(input_center + centered_max_location,
+                                         dtype=int)
+
+            # TODO: replace this with self.norb_image_shape, once you start
+            # setting that using model.input_space
+            norb_image_shape = numpy.asarray((96, 96), dtype=int) #self.norb_image_shape
+            max_min_corner = max_location - norb_image_shape // 2
+            max_max_corner = max_min_corner + norb_image_shape
+            self.model_input_pixels[...] = \
+                self.video_pixels[max_min_corner[0]:max_max_corner[0],
+                                  max_min_corner[1]:max_max_corner[1]]
+
+            # Draw a red square around the object bounding box.
+            cv2.rectangle(self.video_pixels,
+                          tuple(reversed(self.object_min_corner)),
+                          tuple(reversed(self.object_max_corner)),
+                          (255, 0, 0),  # color
+                          2)  # thickness
+
+            softmax_vector = softmax_map[no_bkg_max_location[0],
+                                         no_bkg_max_location[1],
+                                         :]
+
+            # TODO: This whole chunk was copied from ClassifierWindow.
+            #       Refactor to avoid this code duplication.
+            num_candidates = len(self.candidate_pixels_list)
+            sorted_ids = \
+                numpy.argsort(softmax_vector)[-1:-(num_candidates + 1):-1]
+            message = "Category/probability: "
+            for (sorted_id,
+                 candidate_pixels,
+                 probability_pixels) in safe_zip(sorted_ids,
+                                                 self.candidate_pixels_list,
+                                                 self.probability_pixels_list):
+                category = new_norb.get_category_value(sorted_id / 10)
+                instance = sorted_id % 10
+                probability = softmax_vector[sorted_id]
+
+                example_image = self.example_images[sorted_id]
+                candidate_pixels[...] = example_image[:, :, numpy.newaxis]
+
+                # Draws probabilities
+                # Reference: http://docs.opencv.org/trunk/doc/py_tutorials/py_gui/py_drawing_functions/py_drawing_functions.html
+                probability_pixels.fill(0)
+                cv2.putText(probability_pixels,
+                            "%.02f" % probability,
+                            (15, 40),  # position x, y (+x = right, +y = down)
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1,  # font scale
+                            (255, 255, 255), # color
+                            2,  # thickness
+                            cv2.CV_AA)  # line type
+
+        cv2.imshow('classifier', self.all_pixels)
+
+        image_path = os.path.join(self.output_dir,
+                                  "frame_%05d.png" % self.frame_number)
+        cv2.imwrite(image_path, self.all_pixels)
+        self.frame_number += 1
+
+
 def main():
 
     # turns on interactive mode, which enables the draw() function
@@ -384,10 +531,16 @@ def main():
     print("Press 'q' to quit.")
 
     displays = []
-    displays.append(VideoDisplay() if args.model is None
-                    else ClassifierDisplay(args.model,
-                                           args.preprocessor,
-                                           args.scale))
+    if args.model is None:
+        displays.append(VideoDisplay())
+    elif args.localize:
+        displays.append(LocalizerDisplay(args.model,
+                                         args.preprocessor,
+                                         args.scale))
+    else:
+        displays.append(ClassifierDisplay(args.model,
+                                          args.preprocessor,
+                                          args.scale))
 
     if args.matplotlib:
         displays.append(MatplotlibDisplay())
